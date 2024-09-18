2d20960592a172b61af1bb618ac8466ea1c7d0f4
Thomas Cheezum <techeezum@gmail.com>
2024-09-18 18:43:10 -0400

    Delete FluidTagItemHelper, replace with IFluidTagItem

--- src/main/java/com/beesechurger/flyingfamiliars/item/common/fluid_items/BaseFluidTagItem.java
+++ src/main/java/com/beesechurger/flyingfamiliars/item/common/fluid_items/BaseFluidTagItem.java
@@ -1,14 +1,13 @@
 package com.beesechurger.flyingfamiliars.item.common.fluid_items;
 
 import com.beesechurger.flyingfamiliars.item.BaseExtraTagItem;
-import com.beesechurger.flyingfamiliars.tags.FluidTagItemHelper;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
-public abstract class BaseFluidTagItem extends BaseExtraTagItem
+public abstract class BaseFluidTagItem extends BaseExtraTagItem implements IFluidTagItem
 {
     public BaseFluidTagItem(Properties properties)
     {
@@ -49,7 +48,7 @@ public abstract class BaseFluidTagItem extends BaseExtraTagItem
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
     {
         ItemStack stack = player.getItemInHand(hand);
-        FluidTagItemHelper.INSTANCE.ensureTagPopulated(stack);
+        ensureTagPopulated(stack);
 
         return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
     }
